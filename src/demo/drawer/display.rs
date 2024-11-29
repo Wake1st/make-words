b5713d514d5edaa8c93b8ba478c621569937d0fa
Wake1st <joeltwakefield@gmail.com>
2024-11-28 17:45:39 -0700

    added ui; interaction is WIP

--- src/demo/drawer/display.rs
+++ src/demo/drawer/display.rs
@@ -1,5 +1,109 @@
 use bevy::prelude::*;
 
+use crate::{demo::letters::letter_loader::LetterList, screens::Screen, AppSet};
+
+use super::interaction::{CloseDrawer, OpenDrawer};
+
+const UI_BASE_WIDTH: f32 = 288.;
+
 pub(super) fn plugin(app: &mut App) {
-    //
+    app.add_systems(Startup, setup_drawer)
+        .add_systems(Update, (show_drawer, hide_drawer).in_set(AppSet::Update));
+}
+
+#[derive(Component)]
+pub struct LetterDrawer {
+    pub target_position: Vec2,
+}
+
+fn setup_drawer(
+    mut commands: Commands,
+    letter_list: Res<LetterList>,
+    asset_server: Res<AssetServer>,
+) {
+    commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    // fill the entire window
+                    width: Val::Percent(100.),
+                    height: Val::Percent(100.),
+                    flex_direction: FlexDirection::Row,
+                    justify_content: JustifyContent::FlexStart,
+                    align_items: AlignItems::FlexStart,
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            StateScoped(Screen::Gameplay),
+        ))
+        .with_children(|builder| {
+            builder
+                .spawn((
+                    NodeBundle {
+                        style: Style {
+                            flex_direction: FlexDirection::Column,
+                            height: Val::Px(UI_BASE_WIDTH),
+                            width: Val::Percent(100.),
+                            ..Default::default()
+                        },
+                        ..Default::default()
+                    },
+                    StateScoped(Screen::Gameplay),
+                ))
+                .with_children(|builder| {
+                    for letter in letter_list.letters.iter() {
+                        let texture: Handle<Image> =
+                            asset_server.load(format!("images/letters/{}", letter.asset_path));
+
+                        builder.spawn((
+                            letter.clone(),
+                            ButtonBundle {
+                                style: Style {
+                                    width: Val::Px(256.),
+                                    height: Val::Px(256.),
+                                    justify_content: JustifyContent::Center,
+                                    align_items: AlignItems::Center,
+                                    ..default()
+                                },
+                                transform: Transform::from_scale(Vec2::splat(2.0).extend(0.0)),
+                                image: UiImage {
+                                    texture,
+                                    ..default()
+                                },
+                                ..default()
+                            },
+                            StateScoped(Screen::Gameplay),
+                        ));
+                    }
+                });
+        });
+}
+
+fn show_drawer(
+    mut open_drawer: EventReader<OpenDrawer>,
+    mut drawer_query: Query<(&mut LetterDrawer, &Transform)>,
+) {
+    for _ in open_drawer.read() {
+        if let Ok((mut drawer, transform)) = drawer_query.get_single_mut() {
+            drawer.target_position = transform.translation.xy() + Vec2::new(UI_BASE_WIDTH, 0.0);
+        }
+    }
+}
+
+fn hide_drawer(
+    mut close_drawer: EventReader<CloseDrawer>,
+    mut drawer_query: Query<(&mut LetterDrawer, &Transform)>,
+) {
+    for _ in close_drawer.read() {
+        if let Ok((mut drawer, transform)) = drawer_query.get_single_mut() {
+            drawer.target_position = transform.translation.xy() - Vec2::new(UI_BASE_WIDTH, 0.0);
+        }
+    }
+}
+
+fn slide_drawer(mut drawer_query: Query<(&mut Transform, &LetterDrawer)>, time: Res<Time>) {
+    if let Ok((mut transform, drawer)) = drawer_query.get_single_mut() {
+        transform.translation += drawer.target_position.extend(0.0) + time.delta_seconds();
+    }
 }
--- src/demo/drawer/interaction.rs
+++ src/demo/drawer/interaction.rs
@@ -1,5 +1,69 @@
-use bevy::prelude::*;
+use bevy::{input::common_conditions::input_just_pressed, prelude::*};
+
+use crate::{
+    demo::letters::letter::{Letter, SpawnLetter},
+    AppSet,
+};
 
 pub(super) fn plugin(app: &mut App) {
-    //
+    app.add_event::<OpenDrawer>();
+    app.add_event::<CloseDrawer>();
+
+    app.insert_resource(DrawerState { opened: true });
+    app.add_systems(
+        Update,
+        (
+            open_drawer
+                .in_set(AppSet::RecordInput)
+                .run_if(input_just_pressed(KeyCode::Tab)),
+            letter_selected.in_set(AppSet::RecordInput),
+        ),
+    );
+}
+
+#[derive(Event)]
+pub struct OpenDrawer {}
+
+#[derive(Event)]
+pub struct CloseDrawer {}
+
+#[derive(Resource)]
+pub struct DrawerState {
+    pub opened: bool,
+}
+
+fn open_drawer(
+    mut drawer_state: ResMut<DrawerState>,
+    mut open_drawer: EventWriter<OpenDrawer>,
+    mut close_drawer: EventWriter<CloseDrawer>,
+) {
+    //  set new statef
+    drawer_state.opened = !drawer_state.opened;
+
+    //  send the display event
+    if drawer_state.opened {
+        open_drawer.send(OpenDrawer {});
+    } else {
+        close_drawer.send(CloseDrawer {});
+    }
+}
+
+fn letter_selected(
+    mut interaction_query: Query<
+        (&Interaction, &Transform, &Letter),
+        (Changed<Interaction>, With<Letter>),
+    >,
+    mut spawn_letter: EventWriter<SpawnLetter>,
+) {
+    for (interaction, transform, letter) in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                spawn_letter.send(SpawnLetter {
+                    letter: letter.clone(),
+                    position: transform.translation.xy(),
+                });
+            }
+            _ => (),
+        }
+    }
 }
