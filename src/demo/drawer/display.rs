e07eca34ee956e97ae7fb2c847c58d56b03536fb
Wake1st <joeltwakefield@gmail.com>
2024-11-28 17:51:11 -0700

    WIP

--- src/demo/drawer/display.rs
+++ src/demo/drawer/display.rs
@@ -7,8 +7,12 @@ use super::interaction::{CloseDrawer, OpenDrawer};
 const UI_BASE_WIDTH: f32 = 288.;
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_systems(Startup, setup_drawer)
-        .add_systems(Update, (show_drawer, hide_drawer).in_set(AppSet::Update));
+    app.add_systems(Startup, setup_drawer).add_systems(
+        Update,
+        ((show_drawer, hide_drawer), slide_drawer)
+            .chain()
+            .in_set(AppSet::Update),
+    );
 }
 
 #[derive(Component)]
@@ -104,6 +108,17 @@ fn hide_drawer(
 
 fn slide_drawer(mut drawer_query: Query<(&mut Transform, &LetterDrawer)>, time: Res<Time>) {
     if let Ok((mut transform, drawer)) = drawer_query.get_single_mut() {
-        transform.translation += drawer.target_position.extend(0.0) + time.delta_seconds();
+        let direction = (drawer.target_position.extend(0.0) - transform.translation).normalize();
+        let adjustment = direction + time.delta_seconds();
+        if drawer
+            .target_position
+            .extend(0.0)
+            .distance(transform.translation)
+            > adjustment.length()
+        {
+            transform.translation += direction + time.delta_seconds();
+        } else {
+            transform.translation = drawer.target_position.extend(0.0);
+        }
     }
 }
