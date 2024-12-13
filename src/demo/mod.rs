31dabb492f146ca8c53568f33e4e82f26ed09673
Wake1st <joeltwakefield@gmail.com>
2024-12-13 16:15:20 -0700

    i dont need it

--- src/demo/mod.rs
+++ src/demo/mod.rs
@@ -3,15 +3,11 @@ use bevy::prelude::*;
 mod dnd;
 pub mod drawer;
 mod letters;
-pub mod level;
 mod movement;
-pub mod player;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins((
         movement::plugin,
-        player::plugin,
-        level::plugin,
         dnd::plugin,
         letters::plugin,
         drawer::plugin,
